fc095f0cac12833f3074c1d18020c62c45a1026d
BigEvilGreenBean <172852582+BigEvilGreenBean@users.noreply.github.com>
2026-06-07 16:25:10 -0700

    Added some basic helper functions

--- SharedProject/SharedSource/HelperFunctions.cs
+++ SharedProject/SharedSource/HelperFunctions.cs
@@ -1,5 +1,38 @@
 ﻿
+using Barotrauma;
+
 namespace Neurotrauma
 {
+    public class HF // The HelperFunctions class
+    {
+        static public Limb GetCharacterLimb(Character Character, LimbType GivenLimbType)
+        {
+            if (Character == null) { return Character!.AnimController.GetLimb(LimbType.Torso); }
+            return Character!.AnimController.GetLimb(GivenLimbType);
+        }
 
+        static public bool HasAffliction(Character Character, string Identifier = "", float MinAmount = 0)
+        {
+            if (Character == null || Identifier == "") { return false; }
+            Affliction Aff = Character.CharacterHealth.GetAffliction(Identifier);
+            if (Aff == null) { return false; } // Is the affliction null?
+            float AffStrength = Aff.Strength;
+            if (AffStrength > MinAmount)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static public float GetAfflictionStrength(Character Character, String Identifier = "", float DefaultValue = 0)
+        {
+            if (Character != null & Identifier != "")  // Verify we have the info needed.
+            {
+                if (!HasAffliction(Character, Identifier)) { return DefaultValue; }
+                float Strength = Character.CharacterHealth.GetAfflictionStrength(Identifier, GetCharacterLimb(Character, LimbType.Torso), false);
+                return Strength;
+            }
+            return DefaultValue;
+        }
+    }
 }
