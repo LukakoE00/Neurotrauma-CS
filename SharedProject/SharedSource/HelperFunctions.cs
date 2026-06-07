4589f66200a3a1373ef857704bdc1cda1734ce5b
BigEvilGreenBean <172852582+BigEvilGreenBean@users.noreply.github.com>
2026-06-07 16:54:10 -0700

    Added a few more HF Funcs

--- SharedProject/SharedSource/HelperFunctions.cs
+++ SharedProject/SharedSource/HelperFunctions.cs
@@ -1,9 +1,11 @@
 ﻿
 using Barotrauma;
+// What shit was Tina/Mannatu smoking. How do you write this many functions.
+
 
 namespace Neurotrauma
 {
-    public class HF // The HelperFunctions class
+    static public class HF // The HelperFunctions class
     {
         static public Limb GetCharacterLimb(Character Character, LimbType GivenLimbType)
         {
@@ -13,8 +15,8 @@ namespace Neurotrauma
 
         static public bool HasAffliction(Character Character, string Identifier = "", float MinAmount = 0)
         {
-            if (Character == null || Identifier == "") { return false; }
-            Affliction Aff = Character.CharacterHealth.GetAffliction(Identifier);
+            if (Character == null || Identifier == "" || Character.CharacterHealth == null) { return false; }
+            Affliction Aff = GetAffliction(Character,Identifier);
             if (Aff == null) { return false; } // Is the affliction null?
             float AffStrength = Aff.Strength;
             if (AffStrength > MinAmount)
@@ -24,12 +26,46 @@ namespace Neurotrauma
             return false;
         }
 
+        static public bool HasAfflictionLimb(Character Character, string Identifier = "", LimbType GivenLimbType = LimbType.Torso, float MinAmount = 0)
+        {
+            if (Character == null || Identifier == "" || Character.CharacterHealth == null) { return false; }
+            Affliction Aff = GetAfflictionLimb(Character,Identifier,GivenLimbType);
+            if (Aff == null) { return false; } // Is the affliction null?
+            float AffStrength = Aff.Strength;
+            if (AffStrength > MinAmount)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static public Affliction GetAffliction(Character Character, String Identifier = "")
+        {
+            return Character.CharacterHealth.GetAffliction(Identifier); // No error handling on this one, gonna need someone smarter to do that.
+        }
+
+        static public Affliction GetAfflictionLimb(Character Character, String Identifier = "", LimbType GivenLimbType = LimbType.Torso)
+        {
+            return Character.CharacterHealth.GetAffliction(Identifier, GetCharacterLimb(Character, GivenLimbType)); // No error handling on this one, gonna need someone smarter to do that.
+        }
+
         static public float GetAfflictionStrength(Character Character, String Identifier = "", float DefaultValue = 0)
         {
             if (Character != null & Identifier != "")  // Verify we have the info needed.
             {
                 if (!HasAffliction(Character, Identifier)) { return DefaultValue; }
                 float Strength = Character.CharacterHealth.GetAfflictionStrength(Identifier, GetCharacterLimb(Character, LimbType.Torso), false);
+                return Strength;
+            }
+            return DefaultValue;
+        }
+
+        static public float GetAfflictionStrengthLimb(Character Character, LimbType GivenLimbType = LimbType.Torso, String Identifier = "", float DefaultValue = 0)
+        {
+            if (Character != null & Identifier != "")  // Verify we have the info needed.
+            {
+                if (!HasAfflictionLimb(Character,Identifier,GivenLimbType)) { return DefaultValue; }
+                float Strength = Character.CharacterHealth.GetAfflictionStrength(Identifier, GetCharacterLimb(Character, GivenLimbType), false);
                 return Strength;
             }
             return DefaultValue;
