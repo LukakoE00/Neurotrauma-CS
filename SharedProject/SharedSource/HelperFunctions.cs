9ba15cecef5b4b8ca83bae29e2bdd395208d2813
BigEvilGreenBean <172852582+BigEvilGreenBean@users.noreply.github.com>
2026-06-07 21:31:02 -0700

    Added even more HF Funcs. Started NT Compat.

--- SharedProject/SharedSource/HelperFunctions.cs
+++ SharedProject/SharedSource/HelperFunctions.cs
@@ -1,21 +1,55 @@
 ﻿
 using Barotrauma;
-// What shit was Tina/Mannatu smoking. How do you write this many functions.
-
+using FluentResults;
+using System;
+using System.Xml.Linq;
+// What shit was Tina/Mannatu smoking. How do you write this many functions. - GreenBean
+// The majority of these functions are direct translations into C#. Due to the fact that most of the C# methods were already exposed in lua, we can easily do this.
 
 namespace Neurotrauma
 {
-    static public class HF // The HelperFunctions class
+
+    public static class HF // The HelperFunctions class
     {
-        static public Limb GetCharacterLimb(Character Character, LimbType GivenLimbType)
+        // ---------------------------------------- Utility Related Helper Functions -------------------------------------------------- \\
+        public static Limb GetCharacterLimb(Character Character, LimbType GivenLimbType)
         {
-            if (Character == null) { return Character!.AnimController.GetLimb(LimbType.Torso); }
-            return Character!.AnimController.GetLimb(GivenLimbType);
+            return Character.AnimController.GetLimb(GivenLimbType);
         }
 
-        static public bool HasAffliction(Character Character, string Identifier = "", float MinAmount = 0)
+        public static float GetResistance(Character Character, string Identifier, LimbType GivenLimbType = LimbType.Torso) // Only returns health Resistance
         {
-            if (Character == null || Identifier == "" || Character.CharacterHealth == null) { return false; }
+            AfflictionPrefab Prefab = AfflictionPrefab.Prefabs[Identifier];
+            return Character.CharacterHealth.GetResistance(Prefab, GivenLimbType);
+        }
+
+        public static float GetItemAfflictionResistance(Item Item, string ResistanceID) // I thought this was a useful helper function. Credit to Antinous for the original method.
+        {
+            IEnumerable<ContentXElement> ItemElements = Item.Prefab.ConfigElement.Elements();
+            foreach (ContentXElement ItemElement in ItemElements) // Iterate through our elements to find "Wearable"
+            {
+                if (ItemElement.Name == "Wearable")
+                {
+                    foreach (XElement Element in ItemElement.Elements()) 
+                    {
+                        if (Element.Name == "damagemodifier")
+                        {
+                            string Afflictions = Element.GetAttributeString("afflictiontypes","").ToLower();
+                            if (Afflictions.Contains(ResistanceID))
+                            {
+                                return (float) Convert.ToDouble(Element.GetAttributeString("damagemultiplier", "1"));
+                            }
+                        }
+                    }
+                }
+            }
+            return 0; // Womp Womp
+        }
+
+        // ---------------------------------------- Affliction Related Helper Functions -------------------------------------------------- \\
+        public static bool HasAffliction(Character Character, string Identifier = "", float MinAmount = 0)
+        {
+            if (Identifier == "" || Character.CharacterHealth == null) { return false; }
             Affliction Aff = GetAffliction(Character,Identifier);
             if (Aff == null) { return false; } // Is the affliction null?
             float AffStrength = Aff.Strength;
@@ -26,9 +60,9 @@ namespace Neurotrauma
             return false;
         }
 
-        static public bool HasAfflictionLimb(Character Character, string Identifier = "", LimbType GivenLimbType = LimbType.Torso, float MinAmount = 0)
+        public static bool HasAfflictionLimb(Character Character, string Identifier = "", LimbType GivenLimbType = LimbType.Torso, float MinAmount = 0)
         {
-            if (Character == null || Identifier == "" || Character.CharacterHealth == null) { return false; }
+            if (Identifier == "" || Character.CharacterHealth == null) { return false; }
             Affliction Aff = GetAfflictionLimb(Character,Identifier,GivenLimbType);
             if (Aff == null) { return false; } // Is the affliction null?
             float AffStrength = Aff.Strength;
@@ -39,19 +73,19 @@ namespace Neurotrauma
             return false;
         }
 
-        static public Affliction GetAffliction(Character Character, String Identifier = "")
+        public static Affliction GetAffliction(Character Character, String Identifier = "")
         {
             return Character.CharacterHealth.GetAffliction(Identifier); // No error handling on this one, gonna need someone smarter to do that.
         }
 
-        static public Affliction GetAfflictionLimb(Character Character, String Identifier = "", LimbType GivenLimbType = LimbType.Torso)
+        public static Affliction GetAfflictionLimb(Character Character, String Identifier = "", LimbType GivenLimbType = LimbType.Torso)
         {
             return Character.CharacterHealth.GetAffliction(Identifier, GetCharacterLimb(Character, GivenLimbType)); // No error handling on this one, gonna need someone smarter to do that.
         }
 
-        static public float GetAfflictionStrength(Character Character, String Identifier = "", float DefaultValue = 0)
+        public static float GetAfflictionStrength(Character Character, String Identifier = "", float DefaultValue = 0)
         {
-            if (Character != null & Identifier != "")  // Verify we have the info needed.
+            if (Identifier != "")  // Verify we have the info needed.
             {
                 if (!HasAffliction(Character, Identifier)) { return DefaultValue; }
                 float Strength = Character.CharacterHealth.GetAfflictionStrength(Identifier, GetCharacterLimb(Character, LimbType.Torso), false);
@@ -60,9 +94,9 @@ namespace Neurotrauma
             return DefaultValue;
         }
 
-        static public float GetAfflictionStrengthLimb(Character Character, LimbType GivenLimbType = LimbType.Torso, String Identifier = "", float DefaultValue = 0)
+        public static float GetAfflictionStrengthLimb(Character Character, LimbType GivenLimbType = LimbType.Torso, String Identifier = "", float DefaultValue = 0)
         {
-            if (Character != null & Identifier != "")  // Verify we have the info needed.
+            if (Identifier != "")  // Verify we have the info needed.
             {
                 if (!HasAfflictionLimb(Character,Identifier,GivenLimbType)) { return DefaultValue; }
                 float Strength = Character.CharacterHealth.GetAfflictionStrength(Identifier, GetCharacterLimb(Character, GivenLimbType), false);
@@ -70,5 +104,60 @@ namespace Neurotrauma
             }
             return DefaultValue;
         }
+
+        public static void SetAffliction(Character Character, string Identifier, float Strength, Character Aggressor, float PreviousStrength)
+        {
+            SetAfflictionLimb(Character, Identifier, LimbType.Torso ,Strength, Aggressor, PreviousStrength);
+        }
+
+        public static void SetAfflictionLimb(Character Character, string Identifier, LimbType GivenLimbType, float Strength, Character Aggressor, float PreviousStrength)
+        {
+            dynamic Check = AfflictionPrefab.Prefabs.TryGet(Identifier, out AfflictionPrefab Result); // Most likely a better way to acheive this but basically I don't know what this will return.
+            if (Result == null) {return;}
+            AfflictionPrefab Prefab = Result;
+            float Resistance = Character.CharacterHealth.GetResistance(Prefab, GivenLimbType);
+            if (Resistance > 1) {return;}
+            Strength = Strength * Character.CharacterHealth.MaxVitality / 100 / (1 - Resistance);
+            Affliction Affliction = Prefab.Instantiate(Strength, Aggressor);
+            bool RecalculateVitality = NTC.AfflictionsAffectingVitality.Contains(Identifier);
+            Character.CharacterHealth.ApplyAffliction(
+                Character.AnimController.GetLimb(GivenLimbType),
+                Affliction,
+                false,
+                false,
+                RecalculateVitality
+            );
+        }
+
+        public static void AddAfflictionLimb(Character Character, string Identifier, LimbType GivenLimbType, float Strength, Character Aggresor)
+        {
+            if (Strength < 0)
+            {
+                Character.CharacterHealth.ReduceAfflictionOnLimb(
+                    GetCharacterLimb(Character, GivenLimbType),
+                    Identifier,
+                    -Strength,
+                    null,
+                    Aggresor);
+                return;
+            }
+            float PrevStrength = GetAfflictionStrengthLimb(Character, GivenLimbType, Identifier);
+            SetAfflictionLimb(Character, Identifier, GivenLimbType, Strength + PrevStrength, Aggresor, PrevStrength);
+        }
+
+        public static void AddAffliction(Character Character, string Identifier, float Strength, Character Aggresor)
+        {
+            float PrevStrength = GetAfflictionStrength(Character, Identifier);
+            SetAffliction(Character, Identifier, Strength + PrevStrength, Aggresor, PrevStrength);
+        }
+
+        public static void AddAfflictionResisted(Character Character, string Identifier, float Strength, Character Aggresor)
+        {
+            float PrevStrength = GetAfflictionStrength(Character, Identifier);
+            Strength *= 1 - GetResistance(Character, Identifier);
+            SetAffliction(Character, Identifier, Strength + PrevStrength, Aggresor, PrevStrength);
+        }
+
+
     }
 }
---
+++ SharedProject/SharedSource/NTCompat.cs
@@ -0,0 +1,11 @@
+﻿namespace Neurotrauma
+{
+    public static class NTC
+    {
+        public static List<Identifier> AfflictionsAffectingVitality = ["bleeding","bleedingnonstop","burn","acidburn","opiateaddiction",
+                                                                "lacerations","gunshotwound","bitewounds","explosiondamage",
+                                                                "blunttrauma","internaldamage","organdamage","neurotrauma",
+                                                                "gangrene","th_amputation","sh_amputation","alcoholaddiction"];
+    }
+
+}
