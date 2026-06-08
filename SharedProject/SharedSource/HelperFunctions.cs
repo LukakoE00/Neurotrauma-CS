21f1c669e5d073f72b6bfb3feae596c0f9c74323
BigEvilGreenBean <172852582+BigEvilGreenBean@users.noreply.github.com>
2026-06-07 22:26:49 -0700

    MORE FUNNCTIIIIONNNNNSSSS

--- SharedProject/SharedSource/HelperFunctions.cs
+++ SharedProject/SharedSource/HelperFunctions.cs
@@ -3,6 +3,7 @@ using Barotrauma;
 using FluentResults;
 using System;
 using System.Xml.Linq;
+using static Barotrauma.PetBehavior.ItemProduction;
 // What shit was Tina/Mannatu smoking. How do you write this many functions. - GreenBean
 // The majority of these functions are direct translations into C#. Due to the fact that most of the C# methods were already exposed in lua, we can easily do this.
 
@@ -11,11 +12,51 @@ namespace Neurotrauma
 
     public static class HF // The HelperFunctions class
     {
-        // ---------------------------------------- Utility Related Helper Functions -------------------------------------------------- \\
+        // ---------------------------------------- Limb Related Helper Functions -------------------------------------------------- \\
+
+        public static readonly List<LimbType> ArmsLegsToCheck = [LimbType.LeftArm, LimbType.RightArm, LimbType.LeftLeg, LimbType.RightLeg];
+        public static readonly List<LimbType> LegsToCheck = [LimbType.LeftLeg, LimbType.RightLeg];
+        public static readonly List<LimbType> ArmsToCheck = [LimbType.LeftArm, LimbType.RightArm];
+        public static readonly List<LimbType> LimbsToCheck = [LimbType.LeftArm, LimbType.RightArm, LimbType.LeftLeg, LimbType.RightLeg, LimbType.Torso, LimbType.Head];
+
         public static Limb GetCharacterLimb(Character Character, LimbType GivenLimbType)
         {
             return Character.AnimController.GetLimb(GivenLimbType);
         }
+
+        public static LimbType NormalizeLimbType(LimbType GivenLimbType) // Our beloved one and only normalize limb type.
+        {
+            if (LimbsToCheck.Contains(GivenLimbType)) { return GivenLimbType; }
+
+            if (GivenLimbType == LimbType.LeftHand || GivenLimbType == LimbType.LeftForearm)
+            {
+                return LimbType.LeftArm;
+            }
+
+            if (GivenLimbType == LimbType.RightHand || GivenLimbType == LimbType.RightForearm)
+            {
+                return LimbType.RightArm;
+            }
+
+            if (GivenLimbType == LimbType.LeftFoot || GivenLimbType == LimbType.LeftThigh)
+            {
+                return LimbType.LeftLeg;
+            }
+
+            if (GivenLimbType == LimbType.RightFoot || GivenLimbType == LimbType.RightThigh)
+            {
+                return LimbType.RightLeg;
+            }
+
+            if (GivenLimbType == LimbType.Waist)
+            {
+                return LimbType.Torso;
+            }
+
+            return GivenLimbType;
+        }
+
+        // ---------------------------------------- Utility Related Helper Functions -------------------------------------------------- \\
 
         public static float GetResistance(Character Character, string Identifier, LimbType GivenLimbType = LimbType.Torso) // Only returns health Resistance
         {
@@ -23,7 +64,7 @@ namespace Neurotrauma
             return Character.CharacterHealth.GetResistance(Prefab, GivenLimbType);
         }
 
-        public static float GetItemAfflictionResistance(Item Item, string ResistanceID) // I thought this was a useful helper function. Credit to Antinous for the original method.
+        public static float GetItemAfflictionResistance(Barotrauma.Item Item, string ResistanceID) // I thought this was a useful helper function. Credit to Antinous for the original method.
         {
             IEnumerable<ContentXElement> ItemElements = Item.Prefab.ConfigElement.Elements();
             foreach (ContentXElement ItemElement in ItemElements) // Iterate through our elements to find "Wearable"
@@ -43,7 +84,97 @@ namespace Neurotrauma
                     }
                 }
             }
-            return 0; // Womp Womp
+            return 1; // Womp Womp
+        }
+
+        public static float FindDepth(Barotrauma.Item Item) // I butchered this function lol
+        {
+            return Item.WorldPosition.Y * Physics.DisplayToRealWorldRatio;
+        }
+
+        public static Barotrauma.Item GetItemInRightHand(Character Character)
+        {
+            return GetCharacterInventorySlot(Character, 6);
+        }
+
+        public static Barotrauma.Item GetItemInLeftHand(Character Character)
+        {
+            return GetCharacterInventorySlot(Character, 5);
+        }
+
+        public static Barotrauma.Item GetItemInOuterWear(Character Character)
+        {
+            return GetCharacterInventorySlot(Character, 4);
+        }
+
+        public static Barotrauma.Item GetItemInInnerWear(Character Character)
+        {
+            return GetCharacterInventorySlot(Character, 3);
+        }
+
+        public static Barotrauma.Item GetItemInHeadWear(Character Character)
+        {
+            return GetCharacterInventorySlot(Character, 2);
+        }
+
+        public static Barotrauma.Item GetCharacterInventorySlot(Character Character, int Slot)
+        {
+            return Character.Inventory.GetItemAt(Slot);
+        }
+
+        public static string GetCharacterInventorySlotIdentifer(Character Character, int Slot)
+        {
+            Barotrauma.Item Item = GetCharacterInventorySlot(Character, Slot);
+            if (Item == null) { return "null"; }
+            return Item.Prefab.Identifier.Value;
+        }
+
+        public static string GetOuterWearIdentifier(Character Character)
+        {
+            return GetCharacterInventorySlotIdentifer(Character, 4);
+        }
+
+        public static string GetInnerWearIdentifier(Character Character)
+        {
+            return GetCharacterInventorySlotIdentifer(Character, 3);
+        }
+
+        public static string GetHeadWearIdentifier(Character Character)
+        {
+            return GetCharacterInventorySlotIdentifer(Character, 2);
+        }
+
+        public static List<string> EndocrineTalents = [
+            "aggressiveengineering",
+            "crisismanagement",
+            "cannedheat",
+            "doubleduty",
+            "firemanscarry",
+            "fieldmedic",
+            "multitasker",
+            "aceofalltrades",
+            "stillkicking",
+            "drunkensailor",
+            "trustedcaptain",
+            "downwiththeship",
+            "physicalconditioning",
+            "beatcop",
+            "commando",
+            "justascratch",
+            "intheflow",
+            "collegeathletics"];
+        
+        public static void ApplyEndocrineBoost(Character Character, List<string> TalentList = null)
+        {
+            // WIP
+            TalentList = TalentList ?? EndocrineTalents;
+
+            // gee I sure love translating lua into C#
+            Character TargetCharacter = Character;
+            if (TargetCharacter.Info == null) { return; }
+            TalentTree TalentTree = TalentTree.JobTalentTrees[Character.Info.Job.Prefab.Identifier.Value];
+            if (TalentTree == null) { return; }
+            List<string> DisallowedTalents = new List<string>();
         }
 
         // ---------------------------------------- Affliction Related Helper Functions -------------------------------------------------- \\
