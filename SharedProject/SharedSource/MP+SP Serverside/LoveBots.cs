019e4daf1998e9377e6cd7edd16ff6409b0183a0
Luka <ljjk.loos@gmail.com>
2026-06-15 17:31:39 +0200

    Add LoveBots patches for Treatment + Speaking Out

--- SharedProject/SharedSource/MP+SP Serverside/LoveBots.cs
+++ SharedProject/SharedSource/MP+SP Serverside/LoveBots.cs
@@ -1,5 +1,105 @@
 ﻿
+using Barotrauma.Networking;
+
 namespace Neurotrauma
 {
+    public class LoveBots
+    {
+        private static Harmony harmony;
 
+        public static void InitBotPatches()
+        {
+            harmony = new Harmony("NPCPatches");
+
+            DisableBotRescueLogic();
+            SpeakAboutNeurotraumaIssues();
+        }
+
+        public static void DisableBotRescueLogic()
+        {
+            var method_IsValidTarget = AccessTools.Method(typeof(AIObjectiveRescueAll), "IsValidTarget", new[] { typeof(Character), typeof(Character), typeof(bool).MakeByRefType() });
+
+            harmony.Patch(method_IsValidTarget, prefix: new HarmonyMethod(typeof(AIObjectiveRescueAllPatch), nameof(AIObjectiveRescueAllPatch.Prefix_IsValidTarget)));
+        }
+
+        public static void SpeakAboutNeurotraumaIssues()
+        {
+            var method_SpeakAboutIssues = AccessTools.Method(typeof(HumanAIController), "SpeakAboutIssues");
+
+            harmony.Patch(method_SpeakAboutIssues, postfix: new HarmonyMethod(typeof(SpeakAboutIssuesPatch), nameof(SpeakAboutIssuesPatch.Postfix_SpeakAboutIssues)));
+        }
+    }
+
+    public static class AIObjectiveRescueAllPatch
+    {
+        public static bool Prefix_IsValidTarget(Character target, Character healer, ref bool isValid)
+        {
+            if (!NTConfig.Get("NT_disableBotAlgorithms", true))
+            {
+                return true; // Prevent original method from executing
+            }
+
+            isValid = false;
+            return false; // Don't prevent original method from executing.. very important to know!
+        }
+    }
+
+    public static class SpeakAboutIssuesPatch
+    {
+        private static readonly string[] Afflictions =
+        {
+            "n_fracture",
+            "h_arterialcut",
+            "ll_arterialcut",
+            "rl_arterialcut",
+            "ra_arterialcut",
+            "la_arterialcut",
+            "sym_hematemesis",
+            "sym_paleskin",
+            "sym_confusion",
+            "sym_lightheadedness",
+            "pain_abdominal",
+            "inflammation",
+            "gangrene",
+            "fever",
+            "sym_headache",
+            "sym_blurredvision",
+            "t_fracture",
+            "h_fracture",
+            "ra_fracture",
+            "la_fracture",
+            "rl_fracture",
+            "ll_fracture",
+            "dislocation1",
+            "dislocation2",
+            "dislocation3",
+            "dislocation4",
+            "pain_chest",
+            "sym_weakness",
+            "sym_sweating",
+            "dyspnea",
+            "sym_bloating",
+            "sym_legswelling",
+            "sym_craving",
+            "sym_palpitations"
+        };
+
+        public static void Postfix_SpeakAboutIssues(HumanAIController __instance)
+        {
+            Character character = __instance.Character;
+
+            if (!HF.HasAffliction(character, "luabotomy", 1f)) return;
+            ChatMessageType chatType = ChatMessage.CanUseRadio(character) ? ChatMessageType.Radio : ChatMessageType.Default;
+
+            foreach (string identifier in Afflictions)
+            {
+                if (!HF.HasAffliction(character, identifier, 1f)) continue;
+
+                string message = TextManager.Get($"npcdialogsym.{identifier}").Value;
+                character.Speak(message, chatType, Rand.Range(0, 5), new Identifier($"{identifier}DialogSym"), 600f);
+
+                break;
+            }
+        }
+    }
 }
