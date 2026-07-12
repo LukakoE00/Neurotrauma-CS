54bea306753e66bf2f651b48b462a11a3e00663c
BigEvilGreenBean <172852582+BigEvilGreenBean@users.noreply.github.com>
2026-07-12 10:46:12 -0700

    lua fix

--- Assets/Content/Lua/Shared/SharedExample.lua
+++ Assets/Content/Lua/Shared/SharedExample.lua
@@ -38,7 +38,7 @@ LuaUserData.RegisterType("Neurotrauma.HumanUpdate+CharacterStats+NTHumanStatBool
 
 dofile(NT.Path .. "/Lua/Scripts/Shared/ConfigData.lua") 		
 dofile(NT.Path .. "/Lua/Scripts/Shared/HelperFunctions.lua") 		
-dofile(NTServer.Path .. "/Lua/Scripts/Shared/DummyHumanUpdate.lua") 	
-dofile(NTServer.Path .. "/Lua/Scripts/Shared/LegacyAfflictions.lua") 	
-dofile(NTServer.Path .. "/Lua/Scripts/Shared/NTCompat.lua") 	
-dofile(NTServer.Path .. "/Lua/Scripts/Shared/Items.lua") 		
+dofile(NT.Path .. "/Lua/Scripts/Shared/DummyHumanUpdate.lua") 	
+dofile(NT.Path .. "/Lua/Scripts/Shared/LegacyAfflictions.lua") 	
+dofile(NT.Path .. "/Lua/Scripts/Shared/NTCompat.lua") 	
+dofile(NT.Path .. "/Lua/Scripts/Shared/Items.lua") 		
